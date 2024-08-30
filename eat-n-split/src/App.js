b3aa2b6e1380c1b93dc2e5568f670eb1ef77c76f
Suresh Babu <sureshbabu.kanini@gmail.com>
2024-08-30 17:40:32 +0530

    Cleared the Bug in eat-n-split - state updating

--- eat-n-split/src/App.js
+++ eat-n-split/src/App.js
@@ -86,6 +86,7 @@ export default function App() {
         <FormSplitBill
           selectedFriend={selectedFriend}
           onSplitBill={handleSplitBill}
+          key={selectedFriend.id}
         />
       )}
     </div>
