638914a8957b74e8b906c4e32cc76491b7b570aa
Suresh Babu <sureshbabu.kanini@gmail.com>
2024-09-04 11:30:47 +0530

    Added on press Enter search feature

--- usepopcorn/src/Components/Navbar/Search.js
+++ usepopcorn/src/Components/Navbar/Search.js
@@ -1,4 +1,23 @@
+import { useEffect, useRef } from "react";
+
 export default function Search({ query, setQuery }) {
+  const inputEl = useRef(null);
+
+  useEffect(() => {
+    function callBack(e) {
+      if (document.activeElement === inputEl.current) return;
+      if (e.code === "Enter") {
+        inputEl.current.focus();
+        setQuery("");
+      }
+    }
+
+    document.addEventListener("keydown", callBack);
+    return () => {
+      document.addEventListener("keydown", callBack);
+    };
+  }, []);
+
   return (
     <input
       className="search"
@@ -6,6 +25,7 @@ export default function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 }
