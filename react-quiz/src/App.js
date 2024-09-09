46ec61ca2796e6a4afcf201da27fb78b03fbe508
Suresh Babu <sureshbabu.kanini@gmail.com>
2024-09-09 17:28:52 +0530

    Updated in react-quiz -- added reducer methods for functionality

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,13 +1,89 @@
-import Header from "./Header";
-import Main from "./Main";
+import { useEffect, useReducer } from "react";
+import Header from "./Components/Header/Header";
+import Main from "./Components/Layout/Main";
+import Loader from "./Components/Loader";
+import Error from "./Components/Error/Error";
+import StartScreen from "./Components/StartScreen";
+import Questions from "./Components/Questions";
+import NextButton from "./Components/NextButton";
+
+const initialState = {
+  questions: {},
+  //loading, error, ready, active, finished
+  status: "loading",
+  index: 0,
+  answer: null,
+  points: 0,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    case "start":
+      return { ...state, status: "active" };
+    case "newAnswer":
+      const question = state.questions.at(state.index);
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+    case "nextQuestion":
+      return { ...state, index: state.index + 1, answer: null };
+    default:
+      throw new Error("Unknown action");
+  }
+}
 
 export default function App() {
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  const numQuestions = questions.length;
+
+  useEffect(() => {
+    async function fetchQuestions() {
+      try {
+        const response = await fetch(`http://localhost:8000/questions`);
+        if (!response.ok) {
+          throw new Error("Error fetching data");
+        }
+        const data = await response.json();
+        dispatch({ type: "dataReceived", payload: data });
+      } catch (error) {
+        dispatch({ type: "dataFailed" });
+      }
+    }
+    fetchQuestions();
+  }, []);
+
   return (
     <div>
       <Header />
       <Main>
-        <p>1/15</p>
-        <p>Question?</p>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && (
+          <>
+            <Questions
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <NextButton dispatch={dispatch} answer={answer} />
+          </>
+        )}
       </Main>
     </div>
   );
---
+++ react-quiz/src/Components/Questions.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Options from "./Options";
+
+export default function Questions({ question, dispatch, answer }) {
+  return (
+    <div>
+      <h4>{question.question}</h4>
+      <Options question={question} dispatch={dispatch} answer={answer} />
+    </div>
+  );
+}
