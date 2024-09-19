a42da2faf4737afddf0cc7a798d0bf676434ddac
Suresh Babu <sureshbabu.kanini@gmail.com>
2024-09-19 13:21:08 +0530

    Added Bakance display component and store - redux-intro

---
+++ redux-intro/src/store.js
@@ -0,0 +1,35 @@
+import createStore from "react-redux";
+
+const initialState = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case "account/deposit":
+      return { ...state, balance: state.balance + action.payload };
+    case "account/withdraw":
+      return { ...state, balance: state.balance - action.payload };
+    case "account/requestLoan":
+      if (state.loan > 0) return state;
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
